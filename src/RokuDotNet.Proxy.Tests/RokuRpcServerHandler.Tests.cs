a0d00a6b55987dbee7f135ea4df95350e08cc36b
Phillip Hoff <phillip@orst.edu>
2019-01-18 21:38:27 -0800

    Finish query tests.

--- src/RokuDotNet.Proxy.Tests/RokuRpcServerHandler.Tests.cs
+++ src/RokuDotNet.Proxy.Tests/RokuRpcServerHandler.Tests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Moq;
@@ -13,66 +14,91 @@ namespace RokuDotNet.Proxy.Tests
     public sealed class RokuRpcServerHandlerTests
     {
         [Fact]
-        public async Task HandleQueryActiveAppAsync()
+        public Task HandleQueryActiveAppAsync()
         {
-            var expectedDeviceId = "device";
-            var expectedResult = new GetActiveAppResult
-            {
-                ActiveApp = new RokuApp
+            return this.HandleQueryMethodAsync(
+                "query/active-app",
+                d => d.Query.GetActiveAppAsync(It.IsAny<CancellationToken>()),
+                new GetActiveAppResult
                 {
-                    Name = "app"
-                }
-            };
-
-            var device = new Mock<IRokuDevice>(MockBehavior.Strict);
-
-            device
-                .Setup(d => d.Query.GetActiveAppAsync(It.IsAny<CancellationToken>()))
-                .ReturnsAsync(expectedResult);
-
-            Func<string, CancellationToken, Task<IRokuDevice>> deviceMapFunc =
-                (string deviceId, CancellationToken cancellationToken) =>
-                {
-                    Assert.Equal(expectedDeviceId, deviceId);
-
-                    return Task.FromResult(device.Object);
-                };
-
-            var handler = new RokuRpcServerHandler(deviceMapFunc);
-
-            var response = await handler.HandleMethodInvocationAsync(
-                new MethodInvocation
-                {
-                    DeviceId = expectedDeviceId,
-                    MethodName = "query/active-app"
-                });
-
-            device.Verify(d => d.Query.GetActiveAppAsync(It.IsAny<CancellationToken>()), Times.Once);
-            
-            Assert.NotNull(response);
-            Assert.NotNull(response.Payload);
-            Assert.True(JToken.DeepEquals(JToken.FromObject(expectedResult), response.Payload));
-        }
-
-        [Fact]
-        public async Task HandleQueryAppsAsync()
-        {
-            var expectedDeviceId = "device";
-            var expectedResult = new GetAppsResult
-            {
-                Apps = new RokuApp[]
-                {
-                    new RokuApp
+                    ActiveApp = new RokuApp
                     {
                         Name = "app"
                     }
-                }
-            };
+                });
+        }
+
+        [Fact]
+        public Task HandleQueryAppsAsync()
+        {
+            return this.HandleQueryMethodAsync(
+                "query/apps",
+                d => d.Query.GetAppsAsync(It.IsAny<CancellationToken>()),
+                new GetAppsResult
+                {
+                    Apps = new RokuApp[]
+                    {
+                        new RokuApp
+                        {
+                            Name = "app"
+                        }
+                    }
+                });
+        }
+
+        [Fact]
+        public Task HandleQueryDeviceInfoAsync()
+        {
+            return this.HandleQueryMethodAsync(
+                "query/device-info",
+                d => d.Query.GetDeviceInfoAsync(It.IsAny<CancellationToken>()),
+                new DeviceInfo
+                {
+                    IsTv = true
+                });
+        }
+
+        [Fact]
+        public Task HandleQueryTvActiveChannelAsync()
+        {
+            return this.HandleQueryMethodAsync(
+                "query/tv-active-channel",
+                d => d.Query.GetActiveTvChannelAsync(It.IsAny<CancellationToken>()),
+                new GetActiveTvChannelResult
+                {
+                    ActiveChannel = new ActiveTvChannel
+                    {
+                        Name = "PBS"
+                    }
+                });
+        }
+
+        [Fact]
+        public Task HandleQueryTvChannelsAsync()
+        {
+            return this.HandleQueryMethodAsync(
+                "query/tv-channels",
+                d => d.Query.GetTvChannelsAsync(It.IsAny<CancellationToken>()),
+                new GetTvChannelsResult
+                {
+                    Channels = new TvChannel[]
+                    {
+                        new TvChannel
+                        {
+                            Name = "PBS"
+                        }
+                    }
+                });
+        }
+
+        private async Task HandleQueryMethodAsync<TResult>(string methodName, Expression<Func<IRokuDevice, Task<TResult>>> setupFunc, TResult expectedResult)
+        {
+            var expectedDeviceId = "device";
 
             var device = new Mock<IRokuDevice>(MockBehavior.Strict);
 
             device
-                .Setup(d => d.Query.GetAppsAsync(It.IsAny<CancellationToken>()))
+                .Setup(setupFunc)
                 .ReturnsAsync(expectedResult);
 
             Func<string, CancellationToken, Task<IRokuDevice>> deviceMapFunc =
@@ -89,10 +115,10 @@ namespace RokuDotNet.Proxy.Tests
                 new MethodInvocation
                 {
                     DeviceId = expectedDeviceId,
-                    MethodName = "query/apps"
+                    MethodName = methodName
                 });
 
-            device.Verify(d => d.Query.GetAppsAsync(It.IsAny<CancellationToken>()), Times.Once);
+            device.Verify(setupFunc, Times.Once);
             
             Assert.NotNull(response);
             Assert.NotNull(response.Payload);
